6fe2a31b3f78fdeac34f56b27f77a2a9a8bbb6b9
Sineastra <lord.of.light.0002@gmail.com>
2021-01-12 22:56:15 +0200

    added 05_mathOperations

---
+++ 01 Lab - Advanecd Syntax Functions and Statements/05_mathOperations.js
@@ -0,0 +1,12 @@
+function calc(x, y, a) {
+    const operators = {
+        "+": (x, y) => x + y,
+        "-": (x, y) => x - y,
+        "*": (x, y) => x * y,
+        "/": (x, y) => x / y,
+        "%": (x, y) => x % y,
+        "**": (x, y) => x ** y,
+    }
+
+    return operators[a](x, y)
+}
