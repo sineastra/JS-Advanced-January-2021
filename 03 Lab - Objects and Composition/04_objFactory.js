8a47f3c63d5e3d1a4f5d7b356e5a952bae00271b
Sineastra <lord.of.light.0002@gmail.com>
2021-01-19 17:36:52 +0200

    added 04_objFactory.js

---
+++ 03 Lab - Objects and Composition/04_objFactory.js
@@ -0,0 +1,34 @@
+function foo(library, orders) {
+    return orders.reduce((a, v) => {
+        // both ways are 100/100, just wanted to experiment abit with spread and arrays to properties
+
+        const temp = {}
+        temp.name = v.template.name
+        v.parts.forEach(x => {
+            temp[x] = library[x]
+        })
+        a.push(temp)
+        return a
+    }, [])
+    //     a.push({
+    //         name: v.template.name,
+    //         ...v.parts.reduce((a1, v1) => {
+    //             a1[v1] = library[v1]
+    //             return a1
+    //         }, {}),
+    //     })
+    //     return a
+    // }, [])
+}
+
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`)
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`)
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`)
+    },
+}
