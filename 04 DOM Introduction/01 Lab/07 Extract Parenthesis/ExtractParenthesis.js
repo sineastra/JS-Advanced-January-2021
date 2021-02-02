4cb573607dff8962a0681756b5d1f9c999de1ea1
Sineastra <lord.of.light.0002@gmail.com>
2021-02-02 23:17:25 +0200

    added 07 Extract Parenthesis

--- 04 DOM Introduction/01 Lab/07 Extract Parenthesis/ExtractParenthesis.js
+++ 04 DOM Introduction/01 Lab/07 Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,8 @@
 function extract(content) {
+    const target = document.getElementById(content)
 
+    return target.innerHTML
+        .match(/(?!\()[\w*\s*\d*]*(?=\))/g)
+        .filter(x => x !== "")
+        .join("; ")
 }
