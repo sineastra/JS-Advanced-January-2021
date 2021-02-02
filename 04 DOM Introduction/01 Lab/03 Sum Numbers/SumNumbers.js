022591c0366daad576ca3b14cb02e02a1936b228
Sineastra <lord.of.light.0002@gmail.com>
2021-02-02 19:41:37 +0200

    added 03 Sum Numbers

--- 04 DOM Introduction/01 Lab/03 Sum Numbers/SumNumbers.js
+++ 04 DOM Introduction/01 Lab/03 Sum Numbers/SumNumbers.js
@@ -1,3 +1,10 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const html = {
+        getNumField: n => document.getElementById(`num${n}`),
+        result: () => document.getElementById("sum"),
+    }
+    const getNum = e => Number(e.value)
+
+    html.result().value =
+        getNum(html.getNumField(1)) + getNum(html.getNumField(2))
 }
