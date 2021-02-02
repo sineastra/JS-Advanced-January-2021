772f6fdf914788150b7b13a344d468abb6a21850
Sineastra <lord.of.light.0002@gmail.com>
2021-02-02 22:57:54 +0200

    added 06 Sum Table

--- 04 DOM Introduction/01 Lab/06 Sum Table/SumTable.js
+++ 04 DOM Introduction/01 Lab/06 Sum Table/SumTable.js
@@ -1,3 +1,7 @@
 function sumTable() {
+    let dataCells = Array.from(document.getElementsByTagName("td"))
 
+    document.getElementById("sum").innerHTML = dataCells
+        .slice(0, dataCells.length - 1)
+        .reduce((a, v) => a + (Number(v.innerHTML) || 0), 0)
 }
