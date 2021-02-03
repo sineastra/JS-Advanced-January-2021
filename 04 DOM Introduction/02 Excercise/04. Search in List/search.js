cd743f9ceaf778114dd5a62b5d2ec2f7210425f3
Sineastra <lord.of.light.0002@gmail.com>
2021-02-03 13:53:35 +0200

    added 04 Search In List

--- 04 DOM Introduction/02 Excercise/04. Search in List/search.js
+++ 04 DOM Introduction/02 Excercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+    const html = {
+        value: document.getElementById("searchText").value,
+        data: document.getElementById("towns").children,
+        result: document.getElementById("result"),
+    }
+    let count = 0
+
+    Array.from(html.data).map(x => {
+        if (x.innerHTML.includes(html.value)) {
+            x = x.style.textDecoration = "bold underline"
+            count += 1
+        }
+        return x
+    })
+
+    html.result.innerHTML = `${count} matches found`
 }
