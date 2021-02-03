9440a9c3762687e82c87732a886505b1cd657c78
Sineastra <lord.of.light.0002@gmail.com>
2021-02-03 18:27:21 +0200

    added 07 Hell's Kitchen

--- 04 DOM Introduction/02 Excercise/07. Hell's Kitchen/app.js
+++ 04 DOM Introduction/02 Excercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,52 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+    const html = {
+        inputField: document.querySelector("#inputs textarea"),
+        outputBestName: document.querySelector("#bestRestaurant p"),
+        outputBestWorkers: document.querySelector("#workers p"),
+    }
 
-   function onClick () {
-      //   TODO:
-      
-   }
+    const getBest = data =>
+        Object.entries(data).sort(
+            (x, y) => getAverage(y[1]) - getAverage(x[1])
+        )[0]
+
+    const getAverage = workersData =>
+        workersData.reduce((a, v) => a + v[1], 0) / workersData.length
+
+    function deserialize(data) {
+        const getWorkers = data =>
+            data
+                .split(", ")
+                .map(x => x.split(" ").map(y => (isNaN(y) ? y : Number(y))))
+
+        return JSON.parse(data)
+            .map(x => x.split(" - "))
+            .reduce((a, v) => {
+                const [name, workers] = v
+
+                a[name] = a[name]
+                    ? [...a[name], ...getWorkers(workers)]
+                    : getWorkers(workers)
+                return a
+            }, {})
+    }
+
+    function displayResult(data) {
+        let [name, workers] = data
+        workers = workers.sort((x, y) => y[1] - x[1])
+
+        html.outputBestName.innerHTML = `Name: ${name} Average Salary: ${getAverage(
+            workers
+        ).toFixed(2)} Best Salary: ${workers[0][1].toFixed(2)}`
+
+        html.outputBestWorkers.innerHTML = `${workers
+            .map(x => `Name: ${x[0]} With Salary: ${x[1]}`)
+            .join(" ")}`
+    }
+
+    document
+        .getElementById("btnSend")
+        .addEventListener("click", () =>
+            displayResult(getBest(deserialize(html.inputField.value)))
+        )
 }
