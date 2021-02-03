12d5ec0c2003baa1ebabfaf9f1af9209c616903e
Sineastra <lord.of.light.0002@gmail.com>
2021-02-03 15:12:27 +0200

    added 06 Format the Text

--- 04 DOM Introduction/02 Excercise/06. Format the Text/solution.js
+++ 04 DOM Introduction/02 Excercise/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+    const html = {
+        textField: document.getElementById("input"),
+        outputDiv: document.getElementById("output"),
+    }
+    const workArr = html.textField.value
+        .split(".")
+        .filter(x => x.length >= 1)
+        .map(x => x.trim())
+
+    const tagTemplate = (tag, text) => `<${tag}>${text}</${tag}>`
+    let tempIndex = 0
+
+    html.outputDiv.innerHTML = workArr
+        .reduce((a, v, i) => {
+            if (i % 3 === 0 && i !== 0) tempIndex += 1
+            a[tempIndex] = (a[tempIndex] || "") + `${v}.`
+            return a
+        }, [])
+        .map(x => tagTemplate("p", x))
+        .join("\n")
 }
