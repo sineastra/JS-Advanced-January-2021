42f614cc5c0164d44f51769a4da96802d1a7cc36
Sineastra <lord.of.light.0002@gmail.com>
2021-02-03 13:23:06 +0200

    added 02  Pascal or Camel Case

--- 04 DOM Introduction/02 Excercise/02. Pascal or Camel Case/solution.js
+++ 04 DOM Introduction/02 Excercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO...
+    const data = {
+        case: document.getElementById("naming-convention").value,
+        str: document.getElementById("text").value,
+        resultSpan: document.getElementById("result"),
+    }
+    const result = data.str
+        .split(" ")
+        .map(x => x.toLocaleLowerCase())
+        .map(x => `${x.charAt(0).toLocaleUpperCase()}${x.slice(1)}`)
+        .join("")
+
+    if (data.case !== "Camel Case" && data.case !== "Pascal Case") {
+        data.resultSpan.innerHTML = "Error!"
+    } else {
+        data.resultSpan.innerHTML =
+            data.case === "Pascal Case"
+                ? result
+                : `${result.charAt(0).toLocaleLowerCase()}${result.slice(1)}`
+    }
 }
