1cc9b57c43a04accf39cf13d7563707e86450811
Sineastra <lord.of.light.0002@gmail.com>
2021-02-03 13:34:58 +0200

    added 03 Accordion

--- 04 DOM Introduction/02 Excercise/03. Accordion/accordion.js
+++ 04 DOM Introduction/02 Excercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    const html = {
+        button: document.getElementsByClassName("button")[0],
+        content: document.getElementById("extra"),
+    }
+
+    if (html.button.innerHTML === "More") {
+        html.button.innerHTML = "Less"
+        html.content.style.display = "block"
+    } else {
+        html.button.innerHTML = "More"
+        html.content.style.display = "none"
+    }
 }
