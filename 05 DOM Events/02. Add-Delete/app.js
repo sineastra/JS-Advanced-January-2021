b248445b327cee002b809bf96b79d6167cf1f59b
Sineastra <lord.of.light.0002@gmail.com>
2021-02-05 14:27:04 +0200

    added 02 Add Delete

--- 05 DOM Events/02. Add-Delete/app.js
+++ 05 DOM Events/02. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const data = {
+        valueToAdd: document.getElementById("newText").value,
+        list: document.getElementById("items"),
+    }
+
+    function eFactory(tag, content) {
+        const temp = document.createElement(tag)
+        temp.innerHTML = content
+
+        return temp
+    }
+
+    const item = eFactory("li", data.valueToAdd)
+    const deleteLink = eFactory("a", "[Delete]")
+    deleteLink.href = "#"
+    deleteLink.addEventListener("click", e => e.target.parentNode.remove())
+    item.appendChild(deleteLink)
+
+    data.list.appendChild(item)
 }
