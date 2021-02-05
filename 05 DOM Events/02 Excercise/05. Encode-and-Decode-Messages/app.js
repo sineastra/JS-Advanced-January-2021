fe6b96823a77bd3c6c2d43e489a39f3be23cea30
Sineastra <lord.of.light.0002@gmail.com>
2021-02-06 00:42:23 +0200

    added 05 Encode and Decode

--- 05 DOM Events/02 Excercise/05. Encode-and-Decode-Messages/app.js
+++ 05 DOM Events/02 Excercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeField, decodeField] = document.querySelectorAll("textarea")
+
+    const transform = (str, type) =>
+        str
+            .split("")
+            .map(x =>
+                String.fromCharCode(
+                    type === "encode"
+                        ? x.charCodeAt(0) + 1
+                        : x.charCodeAt(0) - 1
+                )
+            )
+            .join("")
+
+    document.addEventListener("click", e => {
+        if (e.target.tagName === "BUTTON") {
+            if (e.target.textContent.includes("Encode")) {
+                decodeField.value = transform(encodeField.value, "encode")
+                encodeField.value = ""
+            } else {
+                decodeField.value = transform(decodeField.value, "decode")
+            }
+        }
+    })
 }
