675c86ca38a79a4d9a490bd054ef223a109eb24c
Sineastra <lord.of.light.0002@gmail.com>
2021-02-05 16:56:49 +0200

    added 05 Highlight Active

--- 05 DOM Events/05. Highlight-Active/app.js
+++ 05 DOM Events/05. Highlight-Active/app.js
@@ -1,3 +1,11 @@
 function focus() {
-    console.log('TODO:...');
+    const inputs = document.getElementsByTagName("input")
+
+    const addFocus = e => (e.parentNode.className = "focused")
+    const removeFocus = e => (e.parentNode.className = "")
+
+    Array.from(inputs).forEach(x => {
+        x.addEventListener("focus", e => addFocus(e.target))
+        x.addEventListener("blur", e => removeFocus(e.target))
+    })
 }
