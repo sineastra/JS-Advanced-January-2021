471dfdb433e6a71befb3eceaed7e78f303192cdf
Sineastra <lord.of.light.0002@gmail.com>
2021-02-05 23:42:48 +0200

    added 03 Locked Profile

--- 05 DOM Events/02 Excercise/03. Locked-Profile/app.js
+++ 05 DOM Events/02 Excercise/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const toggle = (btn, content) => {
+        if (btn.innerHTML === "Show more") {
+            btn.innerHTML = "Hide it"
+            content.style.display = "block"
+        } else {
+            btn.innerHTML = "Show more"
+            content.style.display = "none"
+        }
+    }
+
+    document.getElementById("main").addEventListener("click", e => {
+        if (e.target.tagName === "BUTTON") {
+            const parent = e.target.parentNode
+            const isUnlocked =
+                parent.querySelector("input[type=radio]:checked").value ===
+                "unlock"
+
+            if (isUnlocked) {
+                const infoDiv = parent.querySelector("div")
+                toggle(e.target, infoDiv)
+            }
+        }
+    })
 }
