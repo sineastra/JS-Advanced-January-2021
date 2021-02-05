44009bdf6043302dfc8499612824e2c0cc18c810
Sineastra <lord.of.light.0002@gmail.com>
2021-02-05 23:49:57 +0200

    added 04 Fill Dropdown

--- 05 DOM Events/02 Excercise/04. Fill-Dropdown/dropdown.js
+++ 05 DOM Events/02 Excercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    const html = {
+        text: document.getElementById("newItemText"),
+        value: document.getElementById("newItemValue"),
+        menu: document.getElementById("menu"),
+    }
+
+    const e = document.createElement("option")
+    e.textContent = html.text.value
+    e.value = html.value.value
+    html.text.value = ""
+    html.value.value = ""
+
+    html.menu.appendChild(e)
 }
