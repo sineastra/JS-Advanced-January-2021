7e789d4c8ba39052caacf2c12be6aa512175bb3d
Sineastra <lord.of.light.0002@gmail.com>
2021-02-05 21:28:30 +0200

    added 02 Time Converter

--- 05 DOM Events/02 Excercise/02. Time-Converter/solution.js
+++ 05 DOM Events/02 Excercise/02. Time-Converter/solution.js
@@ -1,4 +1,31 @@
 function attachEventsListeners() {
+    const htmlData = {
+        buttons: Array.from(document.querySelectorAll("input[type='button']")),
+        fields: Array.from(document.querySelectorAll("input[type='text']")),
+    }
 
-    console.log('TODO:...');
+    const fillObj = (seconds, minutes, hours, days) => ({
+        seconds,
+        minutes,
+        hours,
+        days,
+    })
+    const types = {
+        seconds: v => fillObj(v, v / 60, v / 3600, v / 86400),
+        minutes: v => fillObj(v * 60, v, v / 60, v / 1440),
+        hours: v => fillObj(v * 3600, v * 60, v, v / 24),
+        days: v => fillObj(v * 86400, v * 1440, v * 24, v),
+    }
+
+    htmlData.buttons.forEach(x =>
+        x.addEventListener("click", () => {
+            const [typeOfUnits, value] = [
+                x.previousElementSibling.id,
+                x.previousElementSibling.value,
+            ]
+            const data = types[typeOfUnits](value)
+
+            htmlData.fields.map(y => (y.value = data[y.id]))
+        })
+    )
 }
