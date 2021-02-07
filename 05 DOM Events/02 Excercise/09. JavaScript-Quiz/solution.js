9ddb0818c92bbe1a3631c641e6b2d58ed3fec20a
Sineastra <lord.of.light.0002@gmail.com>
2021-02-07 15:23:07 +0200

    added 09 JS quiz

--- 05 DOM Events/02 Excercise/09. JavaScript-Quiz/solution.js
+++ 05 DOM Events/02 Excercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+    const html = {
+        sections: document.getElementsByTagName("section"),
+        outputSection: document.getElementById("results"),
+        outputElement: document.querySelector("#results > li > h1"),
+    }
+    const rightAnswers = [
+        "onclick",
+        "JSON.stringify()",
+        "A programming API for HTML and XML documents",
+    ]
+    const answers = []
+    let i = 0
+
+    document.body.addEventListener("click", e => {
+        if (e.target.className === "answer-text") {
+            const sectionsArr = Array.from(html.sections)
+            sectionsArr[i].style.display = "none"
+
+            if (!rightAnswers.includes(e.target.textContent)) {
+                answers.push(e.target.innerHTML)
+            }
+
+            if (sectionsArr[i + 1] !== undefined) {
+                sectionsArr[i + 1].style.display = "block"
+                i += 1
+            } else {
+                html.outputSection.style.display = "block"
+                html.outputElement.innerHTML =
+                    answers.length > 0
+                        ? `You have ${3 - answers.length} right answers`
+                        : "You are recognized as top JavaScript fan!"
+            }
+        }
+    })
 }
