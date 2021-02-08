a003a9b646a4b109883be713e0263349d49f4c29
Sineastra <lord.of.light.0002@gmail.com>
2021-02-08 12:51:44 +0200

    added 08 Next Article

--- 06 Advanced Functions/02 Exercises/08. Next Article/script.js
+++ 06 Advanced Functions/02 Exercises/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const output = document.getElementById("content")
+    const ourCopy = articles.slice()
+
+    return function showNext(arr = []) {
+        if (ourCopy[0] !== undefined) {
+            const article = document.createElement("article")
+            article.innerHTML = ourCopy.shift()
+            output.appendChild(article)
+        }
+
+        return showNext
+    }
 }
