344aeffb5190b4e12d83a60c567d305f5abcf02a
Sineastra <lord.of.light.0002@gmail.com>
2021-02-08 12:38:52 +0200

    added 07 Simple Calculator

--- 06 Advanced Functions/02 Exercises/07. Simple Calculator/app.js
+++ 06 Advanced Functions/02 Exercises/07. Simple Calculator/app.js
@@ -1,7 +1,21 @@
 function calculator() {
-    // TODO:
+    const html = { s1: "", s2: "", output: "" }
+
+    function calc(a, b, sign) {
+        const signs = { "+": (a, b) => a + b, "-": (a, b) => a - b }
+
+        return signs[sign](Number(a), Number(b))
+    }
+
+    return {
+        init: (a, b, c) => {
+            html.s1 = document.querySelector(a)
+            html.s2 = document.querySelector(b)
+            html.output = document.querySelector(c)
+        },
+        add: () =>
+            (html.output.value = calc(html.s1.value, html.s2.value, "+")),
+        subtract: () =>
+            (html.output.value = calc(html.s1.value, html.s2.value, "-")),
+    }
 }
-
-
-
-
