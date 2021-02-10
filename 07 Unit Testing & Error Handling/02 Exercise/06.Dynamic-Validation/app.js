014b96fae5b5f49d77d5c829b532486e69175e4c
Sineastra <lord.of.light.0002@gmail.com>
2021-02-10 14:33:42 +0200

    added 06 Dynamic Validation

--- 07 Unit Testing & Error Handling/02 Exercise/06.Dynamic-Validation/app.js
+++ 07 Unit Testing & Error Handling/02 Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
-function validate () {
-    // TODO
+function validate() {
+    const input = document.getElementById("email")
+
+    function isValidEmail(str) {
+        if (/^[a-z]+@[a-z]+\.[a-z]+/g.test(str)) return true
+
+        return false
+    }
+
+    function applyStyle(e, email) {
+        e.className = isValidEmail(email) ? "" : "error"
+    }
+    input.addEventListener("change", e => applyStyle(e.target, e.target.value))
 }
