fd556d18fef12a37fb7c4339a7f5152f2e77aa7c
Sineastra <lord.of.light.0002@gmail.com>
2021-02-10 18:16:45 +0200

    added 08 Classes added 01_person.js

---
+++ 08 Classes/01 Lab/01_person.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor (f, l, a, e) {
+        this.firstName = f
+        this.lastName = l
+        this.age = a
+        this.email = e
+    }
+
+    toString = () => `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+}
