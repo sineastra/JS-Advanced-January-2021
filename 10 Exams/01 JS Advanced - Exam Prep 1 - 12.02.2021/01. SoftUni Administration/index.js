691475ae96ef79c7e52baeffd94ce120e2855f67
Sineastra <lord.of.light.0002@gmail.com>
2021-02-15 22:21:43 +0200

    refactored 10. Exams -> 01 -> o1. Softuni Administration

--- 10 Exams/01 JS Advanced - Exam Prep 1 - 12.02.2021/01. SoftUni Administration/index.js
+++ 10 Exams/01 JS Advanced - Exam Prep 1 - 12.02.2021/01. SoftUni Administration/index.js
@@ -1,85 +1,103 @@
-/* I have this feeling every time i write code for the judge that
-its unit tests are weird. Thats why i write the most
-simplified and braindead code i possibly can.
-For my NO-surprise it goes 100/100 every time.
-
-TODO: refactor it.
- */
-
 function solve (e) {
-	const [lectureName, date] = document.querySelectorAll(`input`)
-	const moduleInput = document.getElementsByTagName('select')[0]
-	const modules = document.getElementsByClassName('modules')[0]
+	const html = {
+		lectNameField: document.querySelectorAll(`input`)[0],
+		dateField: document.querySelectorAll(`input`)[1],
+		moduleNameField: document.getElementsByTagName('select')[0],
+		modules: document.getElementsByClassName('modules')[0]
+	}
 
-	const liTemp = (d, l) => {
+	// template for the DOM element for a single lecture - li
+	// we add our property 'date' for easy sorting
+	const lectureTemplate = (date, lectureName) => {
 		const li = document.createElement('li')
 
 		li.className = 'flex'
-		li.innerHTML = `<h4>${l} - ${d}</h4>
+		li.innerHTML = `<h4>${lectureName} - ${date}</h4>
 		<button class='red'>Del</button>`
+		li.date = date
 
 		return li
 	}
 
-	const mainTemp = (n, d, l) => {
-		const wrapper = document.createElement('div')
-		wrapper.className = 'module'
+	// template for the whole module element - div
+	// here we reuse the lecture template but we need to append it
+	// so we can use the .date prop we added to it.
+	const moduleTemplate = (moduleName, date, lectureName) => {
+		const moduleDiv = document.createElement('div')
+		moduleDiv.className = 'module'
+		moduleDiv.innerHTML = `<h3>${moduleName}</h3>`
 
-		wrapper.innerHTML = `<h3>${n}</h3>
-<ul>${liTemp(d, l).outerHTML}</ul>`
+		const moduleLectures = document.createElement('ul')
+		moduleLectures.appendChild(lectureTemplate(date, lectureName))
 
-		return wrapper
+		moduleDiv.appendChild(moduleLectures)
+
+		return moduleDiv
 	}
 
+	// medium-generic fn for a valid input :-D
+	const isValidInpit = (lectureName, date, moduleName) => lectureName !==
+		'' && date !== '' && moduleName !== 'Select module'
+
+
+	// helper functions
 	const formatDate = (s) => s.replace(/-/g, '/').replace('T', ' - ')
 	const formatName = (n) => `${n.toLocaleUpperCase()}-MODULE`
+	const clearInput = () => {
+		html.lectNameField.value = ''
+		html.dateField.value = ''
+		html.moduleNameField.value = 'Select module'
+	}
 
 	document.addEventListener('click', (e) => {
+
+			// on 'Add' Button click
 			if (e.target.tagName === 'BUTTON' && e.target.innerHTML === 'Add') {
 				e.preventDefault()
-				if (lectureName.value !== '' && date.value !== '' &&
-					moduleInput.value !== 'Select module') {
+				const [lectureName, date, moduleName] = [
+					html.lectNameField.value,
+					formatDate(html.dateField.value),
+					formatName(html.moduleNameField.value)
+				]
 
-					const allModules = Array.from(modules.children)
-					const sameIndex = allModules.findIndex(x =>
-						x.childNodes[0].innerHTML.includes(moduleInput.value.toLocaleUpperCase()))
+				if (isValidInpit(lectureName, date, moduleName)) {
+					// if there is added module with the same name ->
+					const sameModule = Array.from(html.modules.children)
+						.find(x => x.children[0].innerHTML === moduleName)
 
-					if (sameIndex !== -1) {
-						let items = Array.from(allModules[sameIndex].children[1].children)
-						items.push(liTemp(formatDate(date.value), lectureName.value))
-						items =
-							items.sort((a, b) => {
-								const firstDate = a.children[0].innerHTML.split(' - ')
-									.slice(1).join(' - ')
-								const secondDate = b.children[0].innerHTML.split(' - ')
-									.slice(1).join(' - ')
+					if (sameModule) {
+						const lecturesContainer = sameModule.children[1]
+						const lectures = Array.from(lecturesContainer.children)
 
-								return firstDate.localeCompare(secondDate)
-							})
-
-						items.forEach(x => allModules[sameIndex].children[1].appendChild(x))
-					} else {
-						modules.appendChild(mainTemp(
-							formatName(moduleInput.value),
-							formatDate(date.value),
-							lectureName.value,
-							)
+						// here we take all lectures, make them array, we push
+						// the new lecture, sort them by date, and append them
+						// to the module section
+						lectures.push(lectureTemplate(date, lectureName))
+						lectures.sort((a, b) => a.date.localeCompare(b.date))
+						lectures.forEach(
+							lecture => lecturesContainer.appendChild(lecture)
 						)
+					} else {
+						// if the module does not exist, we make whole new
+						// module and append it to the modules section.
+						html.modules.appendChild(moduleTemplate(
+							moduleName,
+							date,
+							lectureName
+						))
 					}
-
-					lectureName.value = ''
-					date.value = ''
-					moduleInput.value = 'Select module'
+					clearInput()
 				}
 			}
 
-			if (e.target.tagName === 'BUTTON' && e.target.innerHTML ===
-				'Del') {
+			// on 'Del' button click
+			if (e.target.tagName === 'BUTTON' && e.target.innerHTML === 'Del') {
 				const moduleSection = e.target.parentNode.parentNode.parentNode
 
-				e.target.parentNode.outerHTML = ''
-				if (moduleSection.children[1].children.length === 0) {
-					moduleSection.outerHTML = ''
+				e.target.parentNode.outerHTML = '' // delete lecture element
+
+				if (moduleSection.children[1].children.length === 0) { // if no more lectures in module
+					moduleSection.outerHTML = '' // delete module element
 				}
 			}
 		}
