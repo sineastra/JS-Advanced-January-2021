f4a992099b64e703ed3d9c9164f410fea65a4b7b
Sineastra <lord.of.light.0002@gmail.com>
2021-02-19 16:30:16 +0200

    added 04 Exam Prep 2 15.02.2021 01. Christmas Gifts Delivery.... again

--- 08 Classes/02 Exercise/11. View-Model/app.js
+++ 08 Classes/02 Exercise/11. View-Model/app.js
@@ -1,8 +1,27 @@
 class Textbox {
-    //TODO
+	constructor (selector, regex) {
+		this._value = ''
+		this._elements = document.querySelectorAll(selector)
+		this._invalidSymbols = regex
+	}
+
+	get value () {
+		return this._value
+	}
+
+	set value (v) {
+		this._elements = this._elements.map(x => v)
+		this._value = v
+	}
+
+	get elements () {
+		return this._elements
+	}
+
+	isValid = () => this._invalidSymbols.test(this.value)
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
-let inputs = $('.textbox');
+let textbox = new Textbox('.textbox', /[^a-zA-Z0-9]/)
+let inputs = document.querySelectorAll('.textbox')
 
-inputs.on('input',function(){console.log(textbox.value);});
+inputs.forEach(x => x.addEventListener('input', function () {console.log(x.value)}))
--- test.js
+++ test.js
@@ -1,20 +1,22 @@
-const arr = new Array(10000000).fill(1)
+// const arr = new Array(10000000).fill(1)
+//
+// function reverseBuilt (arr) {
+//     arr = arr.reverse()
+// }
+//
+// function myReverse (arr) {
+//     const myArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         myArr.push(arr.pop())
+//     }
+// }
+//
+// console.time('reverseBuilt')
+// reverseBuilt(arr)
+// console.timeEnd('reverseBuilt')
+//
+// console.time('myReverse')
+// myReverse(arr)
+// console.timeEnd('myReverse')
 
-function reverseBuilt (arr) {
-    arr = arr.reverse()
-}
-
-function myReverse (arr) {
-    const myArr = []
-    for (let i = 0; i < arr.length; i++) {
-        myArr.push(arr.pop())
-    }
-}
-
-console.time('reverseBuilt')
-reverseBuilt(arr)
-console.timeEnd('reverseBuilt')
-
-console.time('myReverse')
-myReverse(arr)
-console.timeEnd('myReverse')
+var1 = 1
